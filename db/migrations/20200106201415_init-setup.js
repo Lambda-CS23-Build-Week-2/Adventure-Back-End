93733df07655f1e93b3dd4f91680d2709a26951f
kingmoc <joe.c.a.garcia@gmail.com>
2020-01-06 20:39:38 -0600

    Fixed Table format

--- db/migrations/20200106201415_init-setup.js
+++ db/migrations/20200106201415_init-setup.js
@@ -1,20 +1,6 @@
 
 exports.up = function(knex) {
-    return knex.schema.createTable('rooms', t => {
-        t.increments();
-        t.integer('room_id')
-          .notNullable()
-          .unique()
-        t.string('type', 255)
-        t.integer('exits')
-          .notNullable()
-          .unsigned()
-          .references('id')
-          .inTable('directions')
-          .onDelete('CASCADE')
-          .onUpdate('CASCADE')
-      })
-        .createTable('directions', t => {
+    return knex.schema.createTable('directions', t => {
           t.increments()
           t.integer('room_id')
             .notNullable()
@@ -26,10 +12,24 @@ exports.up = function(knex) {
             .defaultTo(-2)
           t.integer('west')
             .defaultTo(-2)
+      })
+        .createTable('rooms', t => {
+          t.increments();
+          t.integer('room_id')
+            .notNullable()
+            .unique()
+          t.string('type', 255)
+          t.integer('exits')
+            .notNullable()
+            .unsigned()
+            .references('id')
+            .inTable('directions')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE')
         })
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTableIfExists('rooms')
-  .dropTableIfExists('directions');
+  return knex.schema.dropTableIfExists('directions')
+  .dropTableIfExists('rooms');
 };
